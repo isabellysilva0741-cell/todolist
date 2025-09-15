17a6130307856860c78fdcf16c1b32447153a7bf
isabelly <isabellysilva0741@gmail.com>
2025-09-15 20:01:40 -0300

    atulizar lista

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ const li = document.createElement("li")
 
 li.textContent = "estudar com os materiais do classRoom"
 list.append(li)
+
+list.addEventListener("click", event => {
+   const li = event.target.closest("li")
+
+   if(!li) return
+
+   li.classList.toggle("done")
+})
+
